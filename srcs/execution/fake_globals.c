b9d364c4091da07bf3397d1f9599ae117740177f
bolsakova <tanjutik162@gmail.com>
2024-12-19 17:59:15 +0100

    started to expand the env command

--- srcs/execution/fake_globals.c
+++ srcs/execution/fake_globals.c
@@ -1,20 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit.c                                             :+:      :+:    :+:   */
+/*   fake_globals.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbolsako <tbolsako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/16 14:17:26 by tbolsako          #+#    #+#             */
-/*   Updated: 2024/12/19 15:55:51 by tbolsako         ###   ########.fr       */
+/*   Created: 2024/12/19 17:35:12 by tbolsako          #+#    #+#             */
+/*   Updated: 2024/12/19 17:36:08 by tbolsako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../minishell.h"
-
-// function to terminate the shell
-void	builtin_exit(void)
-{
-	// exit the shell with a status code of 0 (indicating success)
-	exit(0);
-}
+#include "../../minishell.h"
